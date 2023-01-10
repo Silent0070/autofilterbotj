14f1ed8a6cf18d77bc870c0fd053abc6dda791db
Silent0070 <92304473+Silent0070@users.noreply.github.com>
2023-01-10 15:04:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Jnanesh124/v7bot  /v7bot 
+  git clone https://github.com/Silent0070/autofilterbotj  / autofilterbotj
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /v7bot 
+  git clone $UPSTREAM_REPO /autofilterbotj 
 fi
-cd /v7bot 
+cd /autofilterbotj 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
